6fd9d79dde0151ca030ec7457ecd1e4e1a997897
kpujara91 <99332033+kpujara91@users.noreply.github.com>
2024-07-25 14:18:11 +0530

    Update layout.tsx add latest code

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,35 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "BSides Ahmedabad 2024",
-  description: "test",
+  title: `Leading IT Consulting & Software Solutions - Rigved Infotech Pvt Ltd`,
+  description: 'Discover innovative IT consulting and custom software solutions with Rigved Infotech. We provide cutting-edge IT services to drive your business success. Contact us today to transform your IT infrastructure.',
+  authors: [
+    { name: '', url: '' }
+  ],
+  creator: '',
+  publisher: '',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['RigvedIT', 'Jobs', 'rigvedit',"rigvedit.com"],
+  openGraph: {
+    title: `Leading IT Consulting & Software Solutions - Rigved Infotech Pvt Ltd`,
+    description: 'Discover innovative IT consulting and custom software solutions with Rigved Infotech. We provide cutting-edge IT services to drive your business success. Contact us today to transform your IT infrastructure.',
+    images: [
+      `https://rigvedit.com/images/ogimage/Home.png`
+    ]
+  },
+  alternates: {
+    canonical: `https://www.bsidesahmedabad.in/`,
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    'max-image-preview': 'large', 
+    'max-snippet': -1,
+    'max-video-preview': -1,
+  }
 };
 
 export default function RootLayout({
