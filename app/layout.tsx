427ecbb1fc4981cafa423e6a7df895b7c42833f1
kpujara91 <99332033+kpujara91@users.noreply.github.com>
2024-07-25 13:00:22 +0530

    Update layout.tsx : seo testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,26 @@ import { Poppins } from "next/font/google";
 import '@/assets/scss/tailwindcss.scss'
 import '@/assets/scss/global.scss'
 import { Metadata } from "next";
+import { generateMetadata } from 'next';
 
 const poppins = Poppins({
   subsets: ['latin'],
   variable: '--font-poppins',
   weight: ["400"]
 });
-
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata() {
   return {
-    title: 'BSides Ahmedabad 2024',
-    description: 'BSides Ahmedabad 2024',
-    url: 'https://www.bsidesahmedabad.in/',
-    openGraph: {
-      images: ['https://beside-ahmedabad-346o6y360-bsides-ahmedabad.vercel.app/opengraph-image.jpg?a32913bd1aea3970'],
-      alternates: {
-        canonical: 'https://www.bsidesahmedabad.in/',
-        languages: {
-          'en-US': '/en-US',
-        },
+    title: "BSides Ahmedabad 2024",
+    description: "BSides Ahmedabad 2024",
+   openGraph: {
+     title:"BSides Ahmedabad 2024",
+     description:"BSides Ahmedabad 2024",
+       url: 'https://www.bsidesahmedabad.in/',
+        images: ["https://beside-ahmedabad-346o6y360-bsides-ahmedabad.vercel.app/opengraph-image.jpg?a32913bd1aea3970"],
       },
-    },
-  }
+  };
 }
- 
+.
 export default function Page({ params, searchParams }: Props) {}
 
 export default function RootLayout({
