f889ca1269d4e1eb1f9cb2f7b9b18c10edd410d0
kpujara91 <99332033+kpujara91@users.noreply.github.com>
2024-07-25 14:28:57 +0530

    Update next.config.mjs : nextconfig changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,6 @@ const nextConfig = {
     images: {
       loader: "custom",
       loaderFile: './imgixLoader.js',
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'beside-ahmedabad-346o6y360-bsides-ahmedabad.vercel.app'
-          },
-        ],
     },
     webpack(config) {
       config.module.rules.push(
