cfa043e397349edcc14197582fe2996833d495d1
kpujara91 <99332033+kpujara91@users.noreply.github.com>
2024-07-25 14:06:22 +0530

    Update layout.tsx : Add new Meta code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import { Poppins } from "next/font/google";
-import '@/assets/scss/tailwindcss.scss'
-import '@/assets/scss/global.scss'
+import '@/assets/scss/tailwindcss.scss';
+import '@/assets/scss/global.scss';
 import { Metadata } from "next";
-
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -10,22 +9,50 @@ const poppins = Poppins({
   weight: ["400"]
 });
 
-export const metadata: Metadata = {
-  title: "BSides Ahmedabad 2024",
-  description: "BSides Ahmedabad 2024",
-     openGraph: {
-     title:"BSides Ahmedabad 2024",
-     description:"BSides Ahmedabad 2024",
-       url: 'https://www.bsidesahmedabad.in/',
-         images: ["https://beside-ahmedabad-346o6y360-bsides-ahmedabad.vercel.app/opengraph-image.jpg"],
-      },
+interface MetadataParams {
+  params: Record<string, any>;
+  parent?: any;
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export async function generateMetadata({ params, parent }: MetadataParams): Promise<Metadata> {
+  return {
+    title: `Leading IT Consulting & Software Solutions - Rigved Infotech Pvt Ltd`,
+    description: 'Discover innovative IT consulting and custom software solutions with Rigved Infotech. We provide cutting-edge IT services to drive your business success. Contact us today to transform your IT infrastructure.',
+    authors: [
+      { name: '', url: '' }
+    ],
+    creator: '',
+    publisher: '',
+    referrer: 'origin-when-cross-origin',
+    keywords: ['RigvedIT', 'Jobs', 'rigvedit', "rigvedit.com"],
+    openGraph: {
+      title: `Leading IT Consulting & Software Solutions - Rigved Infotech Pvt Ltd`,
+      description: 'Discover innovative IT consulting and custom software solutions with Rigved Infotech. We provide cutting-edge IT services to drive your business success. Contact us today to transform your IT infrastructure.',
+      images: [
+        `https://rigvedit.com/images/ogimage/Home.png`
+      ]
+    },
+    alternates: {
+      canonical: `https://rigvedit.com/`,
+      languages: {
+        'en-US': '/en-US',
+      },
+    },
+    robots: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large', 
+      'max-snippet': -1,
+      'max-video-preview': -1,
+    }
+  };
+}
+
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
   return (
     <html lang="en">
       <body className={`${poppins.variable}`}>{children}</body>
