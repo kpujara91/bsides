b04f5623b3e6c40f6174d51b29f8bd725281ae7c
kpujara91 <99332033+kpujara91@users.noreply.github.com>
2024-07-25 12:14:14 +0530

    Update layout.tsx : Change SEO format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Poppins } from "next/font/google";
-import Head from "next/head";
 import '@/assets/scss/tailwindcss.scss'
 import '@/assets/scss/global.scss'
+import { Metadata } from "next";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -9,11 +9,49 @@ const poppins = Poppins({
   weight: ["400"]
 });
 
-const metadata = {
-  title: "BSides Ahmedabad 2024",
-  description: "",
-  image: "https://beside-ahmedabad-346o6y360-bsides-ahmedabad.vercel.app/opengraph-image.jpg?a32913bd1aea3970"
-};
+export const metadata: Metadata = {
+  // title: 'BSides Ahmedabad 2024',
+  // description: "",
+  // image: "https://beside-ahmedabad-346o6y360-bsides-ahmedabad.vercel.app/opengraph-image.jpg?a32913bd1aea3970"
+
+  title: `BSides Ahmedabad 202 `,
+  description:'',
+  authors: [{ name: '', url: '' }],
+  creator: '',
+  publisher: '',
+  referrer: 'origin-when-cross-origin',
+  // keywords: ['trupartner', 'Jobs', 'TRUPARTNER', 'trupartner.com'],
+  openGraph: {
+    title: `BSides Ahmedabad 2024`,
+    description:'',
+    images: [
+      `https://beside-ahmedabad-346o6y360-bsides-ahmedabad.vercel.app/opengraph-image.jpg?a32913bd1aea3970`
+      // ,...previousImages
+    ],
+  },
+  // alternates: {
+  //     canonical: ``,
+  //     languages: {
+  //       'en-US': '/en-US',
+  //     },
+  //   },
+  robots: {
+      index: true,
+      follow: true,
+       // nocache: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+      'max-video-preview': -1,
+      // googleBot: {
+      //     index: true,
+      //     follow: false,
+      //     noimageindex: true,
+      //     'max-video-preview': -1,
+      //     'max-image-preview': 'large',
+      //     'max-snippet': -1
+      // }
+  }
+}
 
 export default function RootLayout({
   children,
@@ -22,20 +60,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-{/*         <meta
-          name="robots"
-          key="robots-index"
-          content="noindex,nofollow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
-        /> */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.image} />
-      </Head>
       <body className={`${poppins.variable}`}>{children}</body>
     </html>
   );
