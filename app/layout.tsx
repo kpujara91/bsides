a3cc75f9f8625d5d0ef18ad7e6ec5daa717348b9
kpujara91 <99332033+kpujara91@users.noreply.github.com>
2024-07-25 14:14:19 +0530

    Update layout.tsx add old code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import '@/assets/scss/tailwindcss.scss';
-import '@/assets/scss/global.scss';
+import '@/assets/scss/tailwindcss.scss'
+import '@/assets/scss/global.scss'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -8,9 +9,23 @@ const poppins = Poppins({
   weight: ["400"]
 });
 
-export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
+export const metadata: Metadata = {
+  title: "BSides Ahmedabad 2024",
+  description: "test",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
+      <meta
+        name="robots"
+        key="robots-index"
+        content="noindex,nofollow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+      />
       <body className={`${poppins.variable}`}>{children}</body>
     </html>
   );
